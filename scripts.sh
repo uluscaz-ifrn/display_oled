46332619ded4775146d15c642793866bbc2d98e8
Lucas Dantas <162215028+uluscaz-ifrn@users.noreply.github.com>
2025-01-28 18:06:41 -0300

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -91,6 +91,7 @@ for ((i=2; i<=1000; i++)); do
   fi
 done
 
+#Inserção de Dados em Arquivo
 def main():
     with open('pedido.txt', 'a') as arquivo:
         while True:
@@ -102,3 +103,38 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+#!/bin/bash
+
+# Considerando que para um número ser considerado primo ele deve ser divisível somente
+# por 1 e por ele mesmo, implemente em Shell Script a função Primo() e faça um loop com
+# while que solicite um número e execute a função exibindo como resposta se o número é
+# primo ou não até que o usuário digite 0 (zero).
+
+function Primo()
+{
+    Divisivel=0
+    for teste in $(seq $1 -1 1);
+    do
+        if [ $(($1%$teste)) -eq 0 ]; then
+            Divisivel=$(($Divisivel+1))
+        fi
+    done
+    if [ $Divisivel -eq 2 ]; then
+        echo "$1 é Primo"
+    else
+        echo "$1 NÃO é Primo"
+    fi
+}
+
+while :
+do
+    read -p "Digite um Número para Testar (ou 0 para SAIR): " Teste
+    if [ $Teste -eq 0 ]; then
+        break
+    fi
+    Primo $Teste
+done
